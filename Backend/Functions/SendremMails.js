96aa4548450979bef2523df9708616af3f06739f
Rimix-incognito <oluwarimigbadebo@gmail.com>
2022-09-20 15:08:58 +0100

    Another days work

--- Backend/Functions/SendremMails.js
+++ Backend/Functions/SendremMails.js
@@ -4,14 +4,15 @@ const API_KEY = process.env.API_GRID;
 sgMail.setApiKey(API_KEY);
 
 var currentDate  = new Date();
-date1.setHours(24,0,0,0)
+currentDate.setHours(24,0,0,0)
 console.log(currentDate);
 
 //function to check the database for any reminder emails that need to be sent
+// it sends them in bulk 
 nodeCron.schedule('0 0 0 * * *', async() => {
     console.log('ran');
 
-    await Todomodel.find({notification_date:date1, status:'Pending'}).then((data) => {
+    await Todomodel.find({Todo_deadline:currentDate, Todo_status:'Pending'}).then((data) => {
         if(data){
             for(let i=0;i<data.length;i++){
                console.log(data);
@@ -19,7 +20,7 @@ nodeCron.schedule('0 0 0 * * *', async() => {
                const message = {
                    from:{
                        name:'Paydate-Picotel',
-                       email:'oluwarimigbadebo@outlook.com',
+                       email:'oluwarimigbadebo@gmail.com',
                    },
                    to:`${data[i].User_Email}`,
                    subject:`${data[i].title} Notification date has been reached`,
