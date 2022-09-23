bd09260b9ab8d8180062010cdff9db0799a93544
Rimix-incognito <oluwarimigbadebo@gmail.com>
2022-09-23 09:15:42 +0100

    updating components

---
+++ frontend/src/components/dashboardComponent/Confirmdelete.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../Styles/dahsboardStyles/Confirmdelete.css'
+const Confirmdelete = () => {
+  return (
+    <div className="glassmorph fixed w-screen h-screen">
+
+    </div>
+  )
+}
+
+export default Confirmdelete
